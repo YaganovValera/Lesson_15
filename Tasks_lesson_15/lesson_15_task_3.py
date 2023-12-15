a0220d73d61a4ea38cf7b54ca30e9ee2d5c5bb0f
Valera <99064072+YaganovValera@users.noreply.github.com>
2023-12-15 12:06:27 +0400

    Задание №3

---
+++ Tasks_lesson_15/lesson_15_task_3.py
@@ -0,0 +1,183 @@
+import argparse
+import logging
+
+
+class Rectangle:
+    """
+    Класс, представляющий прямоугольник.
+
+    Атрибуты:
+    - width (int): ширина прямоугольника
+    - height (int): высота прямоугольника
+
+    Методы:
+    - perimeter(): вычисляет периметр прямоугольника
+    - area(): вычисляет площадь прямоугольника
+    - __add__(other): определяет операцию сложения двух прямоугольников
+    - __sub__(other): определяет операцию вычитания одного прямоугольника из другого
+    - __lt__(other): определяет операцию "меньше" для двух прямоугольников
+    - __eq__(other): определяет операцию "равно" для двух прямоугольников
+    - __le__(other): определяет операцию "меньше или равно" для двух прямоугольников
+    - __str__(): возвращает строковое представление прямоугольника
+    - __repr__(): возвращает строковое представление прямоугольника, которое может быть использовано для создания нового объекта
+    """
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value < 0:
+            raise ValueError("Ширина должна быть положительной, а не " + str(value))
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("Высота должна быть положительной, а не " + str(value))
+        self._height = value
+
+    def __init__(self, width, height=None):
+        self.width = width
+        if height is None:
+            self.height = width
+        else:
+            self.height = height
+
+    def perimeter(self):
+        """
+        Вычисляет периметр прямоугольника.
+
+        Возвращает:
+        - int: периметр прямоугольника
+        """
+        return 2 * (self.width + self.height)
+
+    def area(self):
+        """
+        Вычисляет площадь прямоугольника.
+
+        Возвращает:
+        - int: площадь прямоугольника
+        """
+        return self.width * self.height
+
+    def __add__(self, other):
+        """
+        Определяет операцию сложения двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - Rectangle: новый прямоугольник, полученный путем сложения двух исходных прямоугольников
+        """
+        width = self.width + other.width
+        perimeter = self.perimeter() + other.perimeter()
+        height = perimeter // 2 - width
+        return Rectangle(width, height)
+
+    def __sub__(self, other):
+        """
+        Определяет операцию вычитания одного прямоугольника из другого.
+
+        Аргументы:
+        - other (Rectangle): вычитаемый прямоугольник
+
+        Возвращает:
+        - Rectangle: новый прямоугольник, полученный путем вычитания вычитаемого прямоугольника из исходного
+        """
+        if self.perimeter() < other.perimeter():
+            self, other = other, self
+        width = abs(self.width - other.width)
+        perimeter = self.perimeter() - other.perimeter()
+        height = perimeter // 2 - width
+        return Rectangle(width, height)
+
+    def __lt__(self, other):
+        """
+        Определяет операцию "меньше" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площадь первого прямоугольника меньше площади второго, иначе False
+        """
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        """
+        Определяет операцию "равно" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площади равны, иначе False
+        """
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        """
+        Определяет операцию "меньше или равно" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площадь первого прямоугольника меньше или равна площади второго, иначе False
+        """
+        return self.area() <= other.area()
+
+    def __str__(self):
+        """
+        Возвращает строковое представление прямоугольника.
+
+        Возвращает:
+        - str: строковое представление прямоугольника
+        """
+        return f"Прямоугольник со сторонами {self.width} и {self.height}"
+
+    def __repr__(self):
+        """
+        Возвращает строковое представление прямоугольника, которое может быть использовано для создания нового объекта.
+
+        Возвращает:
+        - str: строковое представление прямоугольника
+        """
+        return f"Rectangle({self.width}, {self.height})"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Rectangle operations")
+    parser.add_argument("--width1", type=int, help="Width of the first rectangle")
+    parser.add_argument("--height1", type=int, help="Height of the first rectangle")
+    parser.add_argument("--width2", type=int, help="Width of the second rectangle")
+    parser.add_argument("--height2", type=int, help="Height of the second rectangle")
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.NOTSET)
+    try:
+        rectangle1 = Rectangle(args.width1, args.height1)
+        rectangle2 = Rectangle(args.width2, args.height2)
+
+        logging.info("Прямоугольник №1: %s", rectangle1)
+        logging.info("Прямоугольник №2: %s", rectangle2)
+
+        result = rectangle1 + rectangle2
+        logging.info("Сумма прямоугольников: %s", result)
+
+        difference = rectangle1 - rectangle2
+        logging.info("Разница прямоугольников: %s", difference)
+
+    except ValueError as e:
+        logging.error(str(e))
+
+# Введите в терминал:
+# python lesson_15_task_3.py --width1 5 --height1 3 --width2 8 --height2 4
